dd3c1ce51efbff0a3144a8cea0b4cc3a5f8efaa0
guiac <gui.acassemiro@gmail.com>
2023-01-31 19:59:47 -0300

    :wrench: setup docker-compose

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone git@github.com:Remote-Engine-Monitoring/receive_data_service.git
+git clone git@github.com:Remote-Engine-Monitoring/measures_service.git
+git clone git@github.com:Remote-Engine-Monitoring/sensor_service.git
+git clone git@github.com:Remote-Engine-Monitoring/account_service.git
+
+docker-compose up --build -d
