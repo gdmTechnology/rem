9d3c68f4d5c7a160fe25055a93ee8bf2df2aca10
guiac <gui.acassemiro@gmail.com>
2023-02-04 13:58:02 -0300

    :wrench: fix kafka setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 git clone git@github.com:Remote-Engine-Monitoring/receive_data_service.git
 git clone git@github.com:Remote-Engine-Monitoring/measures_service.git
 git clone git@github.com:Remote-Engine-Monitoring/sensor_service.git
