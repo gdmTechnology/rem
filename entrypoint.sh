d5c9373e1e0538bc99c3a42cc55d0007e10f48b4
guiac <gui.acassemiro@gmail.com>
2023-02-08 07:17:19 -0300

    :wrench: setup device_service

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@ git clone git@github.com:Remote-Engine-Monitoring/receive_data_service.git
 git clone git@github.com:Remote-Engine-Monitoring/measures_service.git
 git clone git@github.com:Remote-Engine-Monitoring/sensor_service.git
 git clone git@github.com:Remote-Engine-Monitoring/account_service.git
+git clone git@github.com:Remote-Engine-Monitoring/device_service.git
 
 docker-compose up --build -d
