d076feb50b1d4709ee63464bb63d2dfe6868485a
guiac <gui.acassemiro@gmail.com>
2023-02-25 17:53:37 -0300

    :construction: adding api gateway service

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,6 @@ git clone git@github.com:Remote-Engine-Monitoring/measures_service.git
 git clone git@github.com:Remote-Engine-Monitoring/sensor_service.git
 git clone git@github.com:Remote-Engine-Monitoring/account_service.git
 git clone git@github.com:Remote-Engine-Monitoring/device_service.git
+git clone git@github.com:Remote-Engine-Monitoring/api_gateway_service.git
 
 docker-compose up --build -d
