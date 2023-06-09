bdbbf63e6c73f1495ad8c5b0243a974429d0208e
guiac <gui.acassemiro@gmail.com>
2023-06-09 13:12:35 -0300

    :wrench: add new services in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,8 @@ git clone git@github.com:Remote-Engine-Monitoring/sensor_service.git
 git clone git@github.com:Remote-Engine-Monitoring/account_service.git
 git clone git@github.com:Remote-Engine-Monitoring/device_service.git
 git clone git@github.com:Remote-Engine-Monitoring/api_gateway_service.git
+git clone git@github.com:Remote-Engine-Monitoring/actuator_service.git
+git clone git@github.com:Remote-Engine-Monitoring/broker_mqtt_service.git
+git clone git@github.com:Remote-Engine-Monitoring/command_service.git
 
 docker-compose up --build -d
